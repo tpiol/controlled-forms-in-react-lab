81dddbb8d0847392edcb1d53b8507d8d352f0b3b
Tia Piol <tiarox02@yahoo.com>
2025-08-08 12:07:06 -0700

    exercise 2/3

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -2,6 +2,24 @@ import { useState } from "react";
 
 const Bookshelf = () => {
 
+    const [books, setBooks] = useState([
+        { title: 'Fourth Wing', author: 'Rebecca Yarros' },
+        { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
+    ]);
+    const [newBook, setNewBook] = useState(
+        { title: "", author: "", }
+    );
+
+    const handleInputChange = (event => {
+        setNewBook({ ...newBook, [event.target.name]: event.target.value });
+    });
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setBooks({...books, newBook});
+        setNewBook({ title: "", author: ""});
+    };
+
     return (
 
         <div className="bookshelfDiv">
